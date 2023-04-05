12aec46626dff437bac95f2c5b63367e786ef67f
Joni Lehtiaho <joni.lehtiaho0@gmail.com>
2023-04-05 14:13:05 +0300

    python tuntitehtävä osa2

---
+++ python tuntitehtävä osa2.py
@@ -0,0 +1,9 @@
+print("******************************")
+print("*      VALUUTTAMUUNNIN       *")
+print("******************************")
+kurssi = float(input("Anna dollarin kurssi euroina: "))
+maara = float(input("Anna rahan määrä euroina: "))
+raha = maara / kurssi
+print(f"Rahan arvo on dollareina: {raha:.2f}")
+print("*******************************")
+
